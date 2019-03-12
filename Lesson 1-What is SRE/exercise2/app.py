55cfd2a6fdded30b1138fd73fa370ac71afbb0bd
Gabe <abinante@adobe.com>
2019-03-11 21:51:50 -0700

    major revamp of lesson 1

--- Lesson 1-What is SRE/exercise2/app.py
+++ Lesson 1-What is SRE/exercise2/app.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python3
-from random import seed, choice
+import csv
 from flask import Flask
+import yaml
 import logging
+from prettytable import from_csv
+import json
 
-# Instantiate a logger because we should really use logging once we add some real logic
+# Instantiate a logger because we're taking user input and it can always go bad.
 logger = logging.getLogger(__name__)
-# A seed for randomness
-seed()
-
-# A list of excuses
-excuseslist = ['my car broke down', 'I was at a funeral', 'I had a dentist appointment', 'I had to take the kids to school', 'I was hung over', 'there is terrible traffic today', 'I was dealing with a production outage from home', 'the train was slow' ]
 
 application = Flask(__name__)
 
-@application.route('/excuse')
-def deliver_exuse():
-    return "Sorry, I'm running a bit late today because {}. I will be in shortly!".format(choice(excuseslist))
+@application.route('/list')
+def list_items():
+    item_table = from_csv(open('items.csv')).get_html_string()
+    return item_table
 
+@application.route('/list/json')
+def list_json():
+    with open('items.csv', 'r') as skufile:
+        # read in all the values in the CSV file
+        rowlist = csv.reader(skufile)
+        # Skip the headers for our JSON
+        next(rowlist)
+        # Populate a dictionary with the values
+        dict = {row[0]:[row[1],row[2],row[3]] for row in rowlist}
+    return json.dumps(dict)
+
+@application.route('/SKU/<sku>')
+def list_item(sku: str) -> str:
+    try:
+        with open('items.csv', 'r') as skufile:
+            # read in all the values in the CSV file
+            rowlist = csv.reader(skufile)
+            # Skip the headers for our JSON
+            next(rowlist)
+            # Populate a dictionary with the values
+            dict = {row[0]:[row[1],row[2],row[3]] for row in rowlist}
+            itemsummary = "{}: | Price: ${} | Cost Basis: {}".format(dict[sku][0], dict[sku][1], dict[sku][2])
+    except KeyError as k:
+        logger.error("Failed to load item {}!".format(k))
+        itemsummary = "Error! Please try a numbered SKU!"
+    return itemsummary
 
 @application.route('/ping')
 def health():
